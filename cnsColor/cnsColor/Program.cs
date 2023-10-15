506ae7e3f7792d160eda0a7a67b1b828e08a5575
Анастасия Таганова <nastya.taganova03@mail.ru>
2023-10-15 16:52:55 +0300

    28.Lec

---
+++ cnsColor/cnsColor/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Red;
+
+Console.WriteLine("Зеленый текст на красном фоне");
+Console.ResetColor();
+Console.WriteLine("Обычный текст");
+
+foreach (var color in Enum.GetValues<ConsoleColor>()) {
+Console.WriteLine(color);
+}
---
+++ cnsDictionary/cnsDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿//Словарь
+//Ключ - Значение
+
+//Dictionary<int, string> x = new();
+//x.Add(10, "Liski");
+//x.Add(20, "Sochi");
+//x.Add(40, "Moscow");
+//x.Add(60, "Penza");
+
+Dictionary<int, string> x = new()
+{
+    { 10, "Liski" },
+    { 20, "Sochi" },
+    { 40, "Moscow" },
+    { 60, "Penza" }
+};
+
+Console.WriteLine(x[60]);
+
+x[40] = "Kursk";
+x[5] = "Voronesh";
+
+foreach (var item in x)
+{
+    Console.WriteLine($"key={item.Key}, value={item.Value}");
+}
